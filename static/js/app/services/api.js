ee435bda01cd81da068944d477e20541c3f38256
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2015-04-21 13:48:22 +0300

    seen articles logged to the /next query

--- static/js/app/services/api.js
+++ static/js/app/services/api.js
@@ -3,7 +3,8 @@ SearchApp.service('Api', function($http){
     var params = {
       selected: _.chain(options.results).where({ bookmarked: true }).map(function(bookmark){ return bookmark.id }).value(),
       participant_id: options.participant_id,
-      clicked: _.chain(options.results).where({ clicked: true }).map(function(clicked){ return { id: clicked.id, reading_started: clicked.reading_started.getTime() / 1000, reading_ended: clicked.reading_ended.getTime() / 1000 } }).value()
+      clicked: _.chain(options.results).where({ clicked: true }).map(function(clicked){ return { id: clicked.id, reading_started: clicked.reading_started.getTime() / 1000, reading_ended: clicked.reading_ended.getTime() / 1000 } }).value(),
+      seen: _.chain(options.results).where({ seen: true }).map(function(seen){ return seen.id }).value()
     }
 
     if(options.exploratory == 0 || options.exploratory == 1){
