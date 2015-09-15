538a719c32f33917bda87a0f7f8f28ed63880a0d
Alan Medlar <ajm@lm2-biotek3.biocenter.helsinki.fi>
2015-09-15 11:51:11 +0300

    small fixes for topics API

--- explore/views.py
+++ explore/views.py
@@ -22,7 +22,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView # class-based views
 from rest_framework.decorators import api_view # for function-based views
 
-from explore.models import Article, ArticleTFIDF, Experiment, ExperimentIteration, ArticleFeedback, User
+from explore.models import Article, ArticleTFIDF, Experiment, ExperimentIteration, ArticleFeedback, User, TopicWeight
 from explore.serializers import ArticleSerializer
 from explore.utils import *
 
@@ -676,9 +676,10 @@ def topics(request) :
 
     e = get_experiment(user)
 
-    articles, keyword_stats, article_stats = get_top_articles_linrel(e,
+    articles, keyword_stats, article_stats, stems = get_top_articles_linrel(e,
                                                                      from_article,
                                                                      to_article - from_article,
                                                                      e.exploration_rate)
 
     return Response(get_topics(articles, normalise))
+
